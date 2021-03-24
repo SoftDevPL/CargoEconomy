e073328954f3639b53d83742955b5d08bccc569c
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2021-03-24 11:12:15 +0100

    first commit

---
+++ src/main/java/wg/cargoeco/eco/cargoecocomy/CargoEconomy.java
@@ -0,0 +1,17 @@
+package wg.cargoeco.eco.cargoecocomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CargoEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
