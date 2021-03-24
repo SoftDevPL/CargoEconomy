26bbfbaadcaa8c56e6f53685d98f3f48c8e428ac
KrzysztofProgramming <krzychuk112@gmail.com>
2021-03-24 20:11:59 +0100

    poprawka nazwy komendy

--- src/main/java/wg/cargoeco/eco/cargoecocomy/commands/CommandsManager.java
+++ src/main/java/wg/cargoeco/eco/cargoecocomy/commands/CommandsManager.java
@@ -1,6 +1,5 @@
 package wg.cargoeco.eco.cargoecocomy.commands;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import wg.cargoeco.eco.cargoecocomy.CargoEconomy;
 
@@ -10,7 +9,7 @@ public class CommandsManager {
         new GiveMoneyCommand(CargoEconomy.getInstance().getCommand("givemoney"));
         new ClearMoneyCommand(CargoEconomy.getInstance().getCommand("clearmoney"));
         new WithdrawCommand(CargoEconomy.getInstance().getCommand("withdrawmoney"));
-        new TransferCommand(CargoEconomy.getInstance().getCommand("transfer"));
+        new TransferCommand(CargoEconomy.getInstance().getCommand("transfermoney"));
         new BalanceCommand(CargoEconomy.getInstance().getCommand("balance"));
     }
 
